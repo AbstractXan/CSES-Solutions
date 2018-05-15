8d3cc3e93cdfdeda92de6ad495d3442791d96a75
Priyansh Sangule <priyansh0506@gmail.com>
2018-05-16 05:06:17 +0530

    Update main.cpp

--- 2-Missing-Number/main.cpp
+++ 2-Missing-Number/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
 	ll n,sum,num;
 	cin>>n;
-	sum=n*(n+1)/2;
+	sum=n*(n+1)/2;		//Sum of natrunal numbers till n
 	for (int i=1; i<n; ++i)
-	{ cin>>num; sum-=num;}
-	cout<<sum;
+	{ cin>>num; sum-=num;}	//Subtract input numbers from sum
+	cout<<sum;		//Output will be what remains of sum
 }
