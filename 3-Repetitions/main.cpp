5a3203dcc2c77b12ecae13dd24fea9a1db58bb9d
Priyansh Sangule <priyansh0506@gmail.com>
2018-05-16 05:15:07 +0530

    Update main.cpp

--- 3-Repetitions/main.cpp
+++ 3-Repetitions/main.cpp
@@ -8,12 +8,12 @@ int main(){
 	cin>>s;
 	n = s.size();
 	currmax=1; max=0;
-	for(n=n-1;n>0;n--){ 
-		if(s[n]==s[n-1])
-			{currmax++;
-			if(currmax>max){max=currmax;}}
+	for(n=n-1;n>0;n--){ 		//Traverse from n-1(last) to 1(second)
+		if(s[n]==s[n-1])	//If current==next
+			{currmax++;	//Increment current max
+			if(currmax>max){max=currmax;}} //Check and update max
 		else
-			if(currmax>max){max=currmax; currmax=1;}
+			if(currmax>max){max=currmax; currmax=1;} //If not equal, check and update max, reset current max
 		}
 	cout<<max;
 }
