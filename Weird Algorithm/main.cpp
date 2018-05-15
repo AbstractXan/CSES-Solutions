cee477c4f168d6ab773ebea7c2680d45029e2f00
Priyansh Sangule <priyansh0506@gmail.com>
2018-05-15 21:30:53 +0530

    Update main.cpp

--- Weird Algorithm/main.cpp
+++ Weird Algorithm/main.cpp
@@ -1,1 +1,12 @@
-//Solution here
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+	ll n;
+	cin>>n;
+	cout<<n<<" ";
+	for(;n>1;){
+		n=((n%2==0)?(n/2):(n*3+1));
+		cout<<n<<" ";
+		}
+}
